cbfafcd64c8598c3aedcc9c0f8340e51b6d34985
Tural Agayev <tuaralagayev@gmail.com>
2025-01-29 20:29:14 +0400

    MaxSubarraySum layihəsi əlavə edildi

---
+++ MaxSubarraySum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSubarraySum
+{
+    internal class Program
+    {
+        static int maxSubarraySum(int[] input)
+        {
+            int maxSum = input[0];
+            int currentSum = input[0];
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                currentSum = Math.Max(input[i], currentSum + input[i]);
+
+                maxSum = Math.Max(maxSum, currentSum);
+
+                Console.WriteLine($"i = {i}, input[i] = {input[i]}, currentSum = {currentSum}, maxSum = {maxSum}");
+
+
+            }
+            return maxSum;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4, 7 };
+            int maxSum = maxSubarraySum(nums);
+            Console.WriteLine("Maksimum Subarray Cemi: " + maxSum);
+
+            Console.ReadLine();
+        }
+    }
+}
