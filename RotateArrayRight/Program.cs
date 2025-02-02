361b62e45d58ce71267c573a1ba899ce8c6a3d05
Tural Agayev <tuaralagayev@gmail.com>
2025-02-02 15:18:33 +0400

    RotateArrayRight layihesi elave olundu

---
+++ RotateArrayRight/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateArrayRight
+{
+    internal class Program
+    {
+        static void rotateArrayRight(int[] input, int k)
+        {
+            k = k % input.Length;
+
+            for (int i = 0; i < k; i++)
+            {
+                int t = input[input.Length - 1];
+                for (int j = input.Length - 1; j > 0; j--)
+                {
+                    input[j] = input[j - 1];
+
+                }
+                input[0] = t;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 3;
+            rotateArrayRight(nums, k);
+
+            Console.WriteLine(string.Join(", ", nums));
+
+            Console.ReadLine();
+        }
+    }
+}
