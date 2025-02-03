ee4e20b258f505f3d84e309768fe512773c6f555
Tural Agayev <tuaralagayev@gmail.com>
2025-02-03 21:29:02 +0400

    maxEvenNumber layihesi elave olundu

---
+++ maxEvenNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxEvenNumber
+{
+    internal class Program
+    {
+        static int _maxEvenNumber(int[] input)
+        {
+            int evenMax = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    if (input[i] > evenMax)
+                    {
+                        evenMax = input[i];
+                    }
+                }
+            }
+
+            return evenMax;
+            
+        } 
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 4, 5, 6, 7, 8, 18, 19, 36, 37 };
+            int result = _maxEvenNumber(nums);
+            Console.WriteLine("Massivin en boyuk cut ededi: " + result);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
