7f64a414a9fccebb592f313b333c76c6352b44ce
Tural Agayev <tuaralagayev@gmail.com>
2025-02-16 02:15:01 +0400

    FirstUniqueChar layihesi elave  edildi

---
+++ FirstUniqueChar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstUniqueChar
+{
+    internal class Program
+    {
+        static char? firstUniqueChar(string s)
+        {
+            string lowerS = s.ToLower();
+            for (int i = 0; i < lowerS.Length; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < lowerS.Length; j++)
+                {
+                    if (i != j && lowerS[i] == lowerS[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+
+                if(isUnique)
+                {
+                    return s[i];
+                }
+
+            }
+
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            string str = "Tural";
+            char? result = firstUniqueChar(str);
+
+            if (result.HasValue)
+                Console.WriteLine("İlk unikal simvol: " + result);
+            else
+                Console.WriteLine("Unikal simvol yoxdur.");
+
+            Console.ReadLine();
+        }
+    }
+}
