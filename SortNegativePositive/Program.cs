3502961e2dfd72234d10d30e0e75e2729fe6639f
Tural Agayev <tuaralagayev@gmail.com>
2025-03-02 20:19:15 +0400

    The project has been added.

---
+++ SortNegativePositive/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNegativePositive
+{
+    internal class Program
+    {
+        static int[] SortNegativePositive(int[] input)
+        {
+            List<int> negatives = new List<int>();
+            List<int> positives = new List<int>();
+
+            foreach(var num in input)
+            {
+                if(num < 0)
+                {
+                    negatives.Add(num);
+                }
+                else
+                {
+                    positives.Add(num);
+                }
+
+            }
+
+            negatives.AddRange(positives);
+            return negatives.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] Z = new int[40];
+            Random rnd = new Random();
+
+            for (int i = 0; i < Z.Length; i++)
+            {
+                Z[i] = rnd.Next(-20, 30);
+            }
+
+            Console.WriteLine("Evvelki massiv:");
+            foreach (var item in Z)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            int[] Y = SortNegativePositive(Z);
+            Console.WriteLine("\nDeyisdirilmis massiv:");
+            foreach (var item in Y)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
