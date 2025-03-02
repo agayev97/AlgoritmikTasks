64738970c482a672753e1968c1fcc60f9f6e9030
Tural Agayev <tuaralagayev@gmail.com>
2025-03-03 00:16:28 +0400

    The project has been added.

---
+++ NegativeElementsExtractor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegativeElementsExtractor
+{
+    internal class Program
+    {
+        static int[] negativeElementsExtractor(int[] input)
+        {
+            List<int> negatives = new List<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]  < 0)
+                {
+                    negatives.Add(input[i]);
+                }
+
+            }
+
+            return negatives.ToArray();
+
+        }
+        static void Main(string[] args)
+        {
+            int[] X = new int[20];
+            Random rnd = new Random();
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                X[i] = rnd.Next(-10, 15);
+            }
+
+            Console.WriteLine("Daxil edilmis massiv :");
+            foreach(var item in X)
+            {
+                Console.Write(item + " ");
+            }
+
+            int[] Y = negativeElementsExtractor(X);
+            Console.WriteLine("\nMenfi elementlerden ibaret massiv :");
+            foreach(var item in  Y)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
