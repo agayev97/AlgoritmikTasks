dcb610a99beabc8831a728e9de2598b515c01a36
Tural Agayev <tuaralagayev@gmail.com>
2025-03-16 16:48:03 +0400

    The project has been added.

---
+++ PositivSumCount/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositivSumCount
+{
+    internal class Program
+    {
+        static void positiveSum(int[,] input)
+        {
+       
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                int positivSum = 0;
+                int positvCount = 0;
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i,j] > 0)
+                    {
+                        positivSum = positivSum + input[i, j];
+
+                        positvCount++;
+                    }
+                }
+
+                Console.WriteLine($"Setirin nomresi = {i}, say = {positvCount}, Cem = {positivSum}");
+
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            int[,] A = new int[3, 3]
+            {
+                {8, -2, 2 },
+                {9, -5, 4 },
+                {2, -7, 14 }
+            };
+
+            positiveSum(A);
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+
+}
