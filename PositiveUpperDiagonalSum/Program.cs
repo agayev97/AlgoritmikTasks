611f50d2a02216b9e53fe9b874274ddc66ed59cd
Tural Agayev <tuaralagayev@gmail.com>
2025-03-16 16:47:17 +0400

    The project has been added.

---
+++ PositiveUpperDiagonalSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveUpperDiagonalSum
+{
+    internal class Program
+    {
+        static void UppreDiognalSum(int[,] nums)
+        {
+            for (int i = 0; i < nums.GetLength(0) ; i++)
+            {
+                int upperPostivDiognalSum = 0;
+                int count = 0;
+                for (int j = i + 1; j < nums.GetLength(1); j++)
+                {
+                    if (nums[i,j] > 0)
+                    {
+                        upperPostivDiognalSum += nums[i, j];
+                        count++;
+                    }
+                }
+
+                Console.WriteLine($"Setirin nomresi ={i}, Say = {count}, Cem = {upperPostivDiognalSum}");
+            }
+        }
+
+        static void Main(string[] args)
+        { 
+            int[,] X = new int[6, 6]
+            {
+                {1, 4, -4, -8, 8, 7 },
+                {-2, 4, -4, -11, 6, -7},
+                {3, 4, -4, -8, 5, 9  },
+                {-5, 4, -4, -8, 8, 12},
+                {6, 4, -4, -8, 8, 7  },
+                {-8, 4, -4, -8, 8, 7 }
+            };
+
+            UppreDiognalSum(X);
+
+            Console.ReadLine();
+        }
+    }
+}
