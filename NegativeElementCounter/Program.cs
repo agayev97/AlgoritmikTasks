600db1bcba50c4ade522fabea0a84a2161e8655e
Tural Agayev <tuaralagayev@gmail.com>
2025-03-16 16:45:27 +0400

    The project has been added.

---
+++ NegativeElementCounter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegativeElementCounter
+{
+    internal class Program
+    {
+        static void negativeElementCounter(int[,] input)
+        {
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                int negativeSum = 0;
+                int negativeCount = 0;
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i,j] < 0)
+                    {
+                        negativeSum += input[i, j];
+                        negativeCount++;
+                    }
+                }
+
+                Console.WriteLine($"Setirin nomresi: {i}, Say = {negativeCount}, Cem = {negativeSum}");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] V = new int[3, 4]
+            {
+                {-1, 8, -5, 3},
+                {7,-10, -6, -4},
+                {2, -3, 4, -5},
+            };
+
+            negativeElementCounter(V);
+
+            Console.ReadLine();
+        }
+    }
+}
